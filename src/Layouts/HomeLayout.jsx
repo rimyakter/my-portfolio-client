1331c9ad2fb7b48ecd20a8007c9de42c23de24de
Rimy Akter <rimy@gmail.com>
2025-10-24 14:01:00 +0600

    remove lottie

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -3,21 +3,12 @@ import Home from "../Home/Home";
 import AnimatedProfile from "../Home/AnimatedProfile";
 
 import SocialLinks from "../Home/SocialLinks";
-import Lottie from "lottie-react";
-import animationData from "../assets/lottie/bg-3.json";
 
 import ResumeButton from "../Home/ResumeButton";
 
 const HomeLayout = () => {
   return (
     <div className="relative grid grid-cols-1 md:grid-cols-2 px-5 z-10">
-      {/* Lottie background */}
-      <Lottie
-        animationData={animationData}
-        loop
-        autoplay
-        className="absolute inset-0 w-full h-full object-cover z-0 pointer-events-none"
-      />
       <div className="relative">
         <div>
           <Home></Home>
